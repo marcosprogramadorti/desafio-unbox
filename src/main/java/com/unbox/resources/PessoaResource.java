54871ddbfa579dbf2bcc4dbdff16d91d0029e737
Marcos Tavares <marcosprogramadorti@gmail.com>
2020-09-02 00:36:38 -0300

    Desafio

--- src/main/java/com/unbox/resources/PessoaResource.java
+++ src/main/java/com/unbox/resources/PessoaResource.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unbox.entidades.Pessoa;
@@ -22,47 +21,47 @@ public class PessoaResource {
 	private Servico servico;
 
 	@GetMapping("/listar")
-	public ResponseEntity<Page<Pessoa>> listar(int pagina, int tamanho) {
+	public ResponseEntity<Page<Pessoa>> listar(final int pagina, int tamanho) {
 		tamanho = getTamanhoMinimo(tamanho);
-		Page<Pessoa> lista = servico.listar(PageRequest.of(pagina, tamanho));
+		final Page<Pessoa> lista = servico.listar(PageRequest.of(pagina, tamanho));
 
 		return new ResponseEntity<Page<Pessoa>>(lista, HttpStatus.OK);
 	}
 
 	@GetMapping("/buscarPorId/{id}")
-	public ResponseEntity<Pessoa> buscarPorId(@PathVariable("id") Long id) {
+	public ResponseEntity<Pessoa> buscarPorId(@PathVariable("id") final Long id) {
 
 		return new ResponseEntity<Pessoa>(servico.buscarPorId(id), HttpStatus.OK);
 
 	}
 
 	@GetMapping("/buscarPorCPF/{cpf}")
-	public ResponseEntity<Pessoa> buscarPorId(@PathVariable("cpf") String cpf) {
+	public ResponseEntity<Pessoa> buscarPorId(@PathVariable("cpf") final String cpf) {
 		return new ResponseEntity<Pessoa>(servico.buscarPorCPF(cpf), HttpStatus.OK);
 
 	}
 
 	@GetMapping("/pesquisar")
-	public ResponseEntity<Page<Pessoa>> pesquisar(String nome, int pagina, int tamanho) {
+	public ResponseEntity<Page<Pessoa>> pesquisar(final String nome, final int pagina, int tamanho) {
 		tamanho = getTamanhoMinimo(tamanho);
 
-		Page<Pessoa> lista = servico.pesquisarPorNome(nome, PageRequest.of(pagina, tamanho));
+		final Page<Pessoa> lista = servico.pesquisarPorNome(nome, PageRequest.of(pagina, tamanho));
 
 		return new ResponseEntity<Page<Pessoa>>(lista, HttpStatus.OK);
 
 	}
 
 	@PutMapping("/atualizar")
-	public ResponseEntity<Pessoa> atualizar(Pessoa pessoa) {
-		Pessoa pessoaAtualizada = servico.salvarOuAtualizar(pessoa);
+	public ResponseEntity<Pessoa> atualizar(final Pessoa pessoa) {
+		final Pessoa pessoaAtualizada = servico.salvarOuAtualizar(pessoa);
 		return new ResponseEntity<Pessoa>(pessoaAtualizada, HttpStatus.OK);
 
 	}
 
 	@PostMapping("/salvar")
-	public ResponseEntity<Pessoa> salvar(Pessoa pessoa) {
+	public ResponseEntity<Pessoa> salvar(final Pessoa pessoa) {
 
-		Pessoa pessoaSalva = servico.salvarOuAtualizar(pessoa);
+		final Pessoa pessoaSalva = servico.salvarOuAtualizar(pessoa);
 		return new ResponseEntity<Pessoa>(pessoaSalva, HttpStatus.OK);
 
 	}
